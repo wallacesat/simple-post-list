b38507a9a976666d8bf12ddb84f9cb6dc1474657
Wallace Saturnino <wallace@MacBook-Pro-de-Wallace.local>
2019-08-14 16:30:41 -0300

    Adapt to empty project

--- src/index.js
+++ src/index.js
@@ -1,90 +1,10 @@
-import './config/ReactotronConfig';
-import './config/DevToolsConfig';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  Platform, StyleSheet, ScrollView, View, Text,
-} from 'react-native';
+const postList = () => (
+  <View>
+    <Text>BLABLABLA</Text>
+  </View>
+);
 
-import Post from './Post';
-
-const white = '#FFF';
-const primary = '#EE7777';
-const title = '#333';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: primary,
-  },
-
-  header: {
-    backgroundColor: white,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // No iOS precisamos aplicar 20px de padding superior
-    // já que a Status Bar não joga o conteúdo do App
-    // pra baixo automaticamente
-    ...Platform.select({
-      ios: {
-        height: 76,
-        paddingTop: 20,
-      },
-      android: {
-        height: 56,
-        paddingTop: 0,
-      },
-    }),
-  },
-
-  headerTitle: {
-    color: title,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
-
-export default class App extends Component {
-  state = {
-    posts: [
-      {
-        id: 1,
-        title: 'Aprendendo React Native',
-        author: 'Diego Fernandes',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      },
-      {
-        id: 2,
-        title: 'Aprendendo React Native',
-        author: 'Diego Fernandes',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      },
-      {
-        id: 3,
-        title: 'Aprendendo React Native',
-        author: 'Diego Fernandes',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      },
-    ],
-  };
-
-  render() {
-    const { posts } = this.state;
-
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerTitle}>GoNative App</Text>
-        </View>
-        <ScrollView>
-          {posts.map(post => (
-            <Post key={post.id} data={post} />
-          ))}
-        </ScrollView>
-      </View>
-    );
-  }
-}
+export default postList;
